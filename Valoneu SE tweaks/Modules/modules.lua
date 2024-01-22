5feb69d2198435b5a764059a7919c095293f6bea
Valoneu <filip@becicka.cz>
2024-01-23 00:25:25 +0100

    lines in modules.lua

--- Valoneu SE tweaks/Modules/modules.lua
+++ Valoneu SE tweaks/Modules/modules.lua
@@ -1,189 +1,29 @@
-data.raw.module["productivity-module"].effect =
-  {
-    productivity = {bonus = 0.08},
-    consumption = {bonus = 1},
-    pollution = {bonus = 0.1},
-    speed = {bonus = -0.2}
-  }
+data.raw.module["productivity-module"].effect = {   productivity = {bonus = 0.08}, consumption = {bonus = 1},   pollution = {bonus = 0.1},  speed = {bonus = -0.2} }
+data.raw.module["productivity-module-2"].effect = { productivity = {bonus = 0.12}, consumption = {bonus = 1.2}, pollution = {bonus = 0.12}, speed = {bonus = -0.3} }
+data.raw.module["productivity-module-3"].effect = { productivity = {bonus = 0.16}, consumption = {bonus = 1.6}, pollution = {bonus = 0.16}, speed = {bonus = -0.4} }
+data.raw.module["productivity-module-4"].effect = { productivity = {bonus = 0.2},  consumption = {bonus = 2},   pollution = {bonus = 0.2},  speed = {bonus = -0.5} }
+data.raw.module["productivity-module-5"].effect = { productivity = {bonus = 0.24}, consumption = {bonus = 2.4}, pollution = {bonus = 0.24}, speed = {bonus = -0.6} }
+data.raw.module["productivity-module-6"].effect = { productivity = {bonus = 0.28}, consumption = {bonus = 2.8}, pollution = {bonus = 0.28}, speed = {bonus = -0.7} }
+data.raw.module["productivity-module-7"].effect = { productivity = {bonus = 0.32}, consumption = {bonus = 3.2}, pollution = {bonus = 0.32}, speed = {bonus = -0.8} }
+data.raw.module["productivity-module-8"].effect = { productivity = {bonus = 0.36}, consumption = {bonus = 3.6}, pollution = {bonus = 0.36}, speed = {bonus = -0.90} }
+data.raw.module["productivity-module-9"].effect = { productivity = {bonus = 0.4},  consumption = {bonus = 4},   pollution = {bonus = 0.4},  speed = {bonus = -1} }
 
-data.raw.module["productivity-module-2"].effect =
-  {
-    productivity = {bonus = 0.12},
-    consumption = {bonus = 1.2},
-    pollution = {bonus = 0.12},
-    speed = {bonus = -0.3}
-  }
+data.raw.module["speed-module"].effect = {   speed = {bonus = 0.5}, consumption = {bonus = 1.1},  pollution = {bonus = 0.1} }
+data.raw.module["speed-module-2"].effect = { speed = {bonus = 1},   consumption = {bonus = 2.2},  pollution = {bonus = 0.2}  }
+data.raw.module["speed-module-3"].effect = { speed = {bonus = 2},   consumption = {bonus = 4.4},  pollution = {bonus = 0.4}  }
+data.raw.module["speed-module-4"].effect = { speed = {bonus = 2.5}, consumption = {bonus = 5.5},  pollution = {bonus = 0.5} }
+data.raw.module["speed-module-5"].effect = { speed = {bonus = 3.0}, consumption = {bonus = 7.5},  pollution = {bonus = 0.6} }
+data.raw.module["speed-module-6"].effect = { speed = {bonus = 3.5}, consumption = {bonus = 10},   pollution = {bonus = 0.7} }
+data.raw.module["speed-module-7"].effect = { speed = {bonus = 4},   consumption = {bonus = 13},   pollution = {bonus = 0.8} }
+data.raw.module["speed-module-8"].effect = { speed = {bonus = 4.5}, consumption = {bonus = 16.5}, pollution = {bonus = 0.9} }
+data.raw.module["speed-module-9"].effect = { speed = {bonus = 5},   consumption = {bonus = 20},   pollution = {bonus = 1} }
 
-data.raw.module["productivity-module-3"].effect =
-  {
-    productivity = {bonus = 0.16},
-    consumption = {bonus = 1.6},
-    pollution = {bonus = 0.16},
-    speed = {bonus = -0.4}
-  }
-
-data.raw.module["productivity-module-4"].effect =
-  {
-		productivity = {bonus = 0.2},
-		consumption = {bonus = 2},
-		pollution = {bonus = 0.2},
-		speed = {bonus = -0.5}
-  }
-
-data.raw.module["productivity-module-5"].effect =
-  {
-		productivity = {bonus = 0.24},
-		consumption = {bonus = 2.4},
-		pollution = {bonus = 0.24},
-		speed = {bonus = -0.6}
-  }
-
-data.raw.module["productivity-module-6"].effect =
-  {
-		productivity = {bonus = 0.28},
-		consumption = {bonus = 2.8},
-		pollution = {bonus = 0.28},
-		speed = {bonus = -0.7}
-  }
-
-data.raw.module["productivity-module-7"].effect =
-  {
-		productivity = {bonus = 0.32},
-		consumption = {bonus = 3.2},
-		pollution = {bonus = 0.32},
-		speed = {bonus = -0.8}
-  }
-
-data.raw.module["productivity-module-8"].effect =
-  {
-		productivity = {bonus = 0.36},
-		consumption = {bonus = 3.6},
-		pollution = {bonus = 0.36},
-		speed = {bonus = -0.90}
-  }
-	
-data.raw.module["productivity-module-9"].effect =
-  {
-		productivity = {bonus = 0.4},
-		consumption = {bonus = 4},
-		pollution = {bonus = 0.4},
-		speed = {bonus = -1}
-  }
-	
-	
-data.raw.module["speed-module"].effect =
-  {
-    consumption = {bonus = 1.1},
-    pollution = {bonus = 0.1},
-    speed = {bonus = 0.5}
-  }
-
-data.raw.module["speed-module-2"].effect =
-  {
-    consumption = {bonus = 2.2},
-    pollution = {bonus = 0.2},
-    speed = {bonus = 1}
-  }
-
-data.raw.module["speed-module-3"].effect =
-  {
-    consumption = {bonus = 4.4},
-    pollution = {bonus = 0.4},
-    speed = {bonus = 2}
-  }
-
-	data.raw.module["speed-module-4"].effect =
-  {
-		speed = {bonus = 2.5},
-		consumption = {bonus = 5.5},
-		pollution = {bonus = 0.5}
-  }
-
-data.raw.module["speed-module-5"].effect =
-  {
-		speed = {bonus = 3.0},
-		consumption = {bonus = 7.5},
-		pollution = {bonus = 0.6}
-  }
-
-data.raw.module["speed-module-6"].effect =
-  {
-		speed = {bonus = 3.5},
-		consumption = {bonus = 10},
-		pollution = {bonus = 0.7}
-  }
-
-data.raw.module["speed-module-7"].effect =
-  {
-		speed = {bonus = 4},
-		consumption = {bonus = 13},
-		pollution = {bonus = 0.8}
-  }
-
-data.raw.module["speed-module-8"].effect =
-  {
-		speed = {bonus = 4.5},
-		consumption = {bonus = 16.5},
-		pollution = {bonus = 0.9}
-  }
-	
-data.raw.module["speed-module-9"].effect =
-  {
-		speed = {bonus = 5},
-		consumption = {bonus = 20},
-		pollution = {bonus = 1}
-  }
-
-data.raw.module["effectivity-module"].effect =
-  {
-    consumption = {bonus = -1},
-    pollution = {bonus = -0.25},
-  }
-
-data.raw.module["effectivity-module-2"].effect =
-  {
-    consumption = {bonus = -1.5},
-    pollution = {bonus = -0.375},
-  }
-
-data.raw.module["effectivity-module-3"].effect =
-  {
-    consumption = {bonus = -2.5},
-    pollution = {bonus = -0.5},
-  }
-
-data.raw.module["effectivity-module-4"].effect =
-  {
-		consumption = {bonus = -4.25},
-		pollution = {bonus = -0.625},
-  }
-
-data.raw.module["effectivity-module-5"].effect =
-  {
-		consumption = {bonus = -6.75},
-		pollution = {bonus = -0.75},
-  }
-
-data.raw.module["effectivity-module-6"].effect =
-  {
-		consumption = {bonus = -10},
-		pollution = {bonus = -0.875},
-  }
-
-data.raw.module["effectivity-module-7"].effect =
-  {
-		consumption = {bonus = -14},
-		pollution = {bonus = -1},
-  }
-
-data.raw.module["effectivity-module-8"].effect =
-  {
-		consumption = {bonus = -19},
-		pollution = {bonus = -1.125}
-  }
-	
-data.raw.module["effectivity-module-9"].effect =
-  {
-		consumption = {bonus = -25},
-		pollution = {bonus = -1.25}
-  }
+data.raw.module["effectivity-module"].effect = {   consumption = {bonus = -1},    pollution = {bonus = -0.25}, }
+data.raw.module["effectivity-module-2"].effect = { consumption = {bonus = -1.5},  pollution = {bonus = -0.375}, }
+data.raw.module["effectivity-module-3"].effect = { consumption = {bonus = -2.5},  pollution = {bonus = -0.5}, }
+data.raw.module["effectivity-module-4"].effect = { consumption = {bonus = -4.25}, pollution = {bonus = -0.625}, }
+data.raw.module["effectivity-module-5"].effect = { consumption = {bonus = -6.75}, pollution = {bonus = -0.75}, }
+data.raw.module["effectivity-module-6"].effect = { consumption = {bonus = -10},   pollution = {bonus = -0.875}, }
+data.raw.module["effectivity-module-7"].effect = { consumption = {bonus = -14},   pollution = {bonus = -1}, }
+data.raw.module["effectivity-module-8"].effect = { consumption = {bonus = -19},   pollution = {bonus = -1.125} }
+data.raw.module["effectivity-module-9"].effect = { consumption = {bonus = -25},   pollution = {bonus = -1.25} }
